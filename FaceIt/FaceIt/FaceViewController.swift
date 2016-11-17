c49f73a403f9952ff1fbb45e6eaf0ea121381c78
Julia Vasenina <mamicheva@gmail.com>
2016-11-17 15:29:00 +0100

    2 - FaceIt created

---
+++ FaceIt/FaceIt/FaceViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  FaceIt
+//
+//  Created by user on 17/11/16.
+//  Copyright © 2016 Vasenina. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+   
+
+
+}
+
