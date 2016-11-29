693e5dfdfdacd61097fd1f72a716ff40b60f9da7
Julia Vasenina <mamicheva@gmail.com>
2016-11-29 22:04:02 +0100

    lection 5 Pangesturerecognizer added

--- FaceIt/FaceIt/FaceViewController.swift
+++ FaceIt/FaceIt/FaceViewController.swift
@@ -18,10 +18,37 @@ class FaceViewController: UIViewController {
     
     @IBOutlet weak var faceView: FaceView!{
         didSet{
-            faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: "changeScale:"))
+            faceView.addGestureRecognizer(UIPinchGestureRecognizer(target: faceView, action: "changeScale:"))//#selector (FaceView.changeScale(_:))
+            let happierSwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: "increaseHappiness")//#selector FaceViewController.increaseHappiness
+            happierSwipeGestureRecognizer.direction = .Up
+            faceView.addGestureRecognizer(happierSwipeGestureRecognizer)
+            
+            let sadderSwipeGestureRecognizer = UISwipeGestureRecognizer(target: self, action: "decreaseHappiness")//#selector FaceViewController.increaseHappiness
+            sadderSwipeGestureRecognizer.direction = .Down
+            faceView.addGestureRecognizer(sadderSwipeGestureRecognizer)
+            
             updateUI()
         }
     }
+    
+    @IBAction func toggleEyes(recognizer: UITapGestureRecognizer) {
+        if recognizer.state == .Ended {
+            switch expression.eyes{
+            case .Open: expression.eyes = .Closed
+            case .Closed: expression.eyes = .Open
+            case .Squinting: break
+            }
+        }
+    }
+    
+    func increaseHappiness(){
+        expression.mouth = expression.mouth.happierMouth()
+    }
+    
+    func decreaseHappiness(){
+        expression.mouth = expression.mouth.sadderMouth()
+    }
+    
     
     private var mouthCurvatures = [FacialExpression.Mouth.Frown: -1.0, .Grin: 0.5, .Smile: 1.0, .Smirk:-0.5, .Neutral: 0.0]
     private var eyeBrowTilts = [FacialExpression.EyeBrows.Relaxed: 0.5, .Furrowed: -0.5, .Normal:0.0]
