d8898b0aa6d58538a6988e2451a3cf46efea4730
Julia Vasenina <mamicheva@gmail.com>
2016-12-14 00:30:12 +0100

    smashtag demo added

---
+++ Smashtag/Smashtag/TweetTableViewCell.swift
@@ -0,0 +1,62 @@
+//
+//  TweetTableViewCell.swift
+//  Smashtag
+//
+//  Created by user on 13/12/2016.
+//  Copyright © 2016 Vasenina. All rights reserved.
+//
+
+import UIKit
+import Twitter
+
+class TweetTableViewCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var tweetProfileImageView: UIImageView!
+    @IBOutlet weak var tweetCreatedLabel: UILabel!
+    @IBOutlet weak var tweetScreenNameLabel: UILabel!
+    @IBOutlet weak var tweetTextLabel: UILabel!
+    
+    var tweet: Twitter.Tweet? {
+        didSet {
+            updateUI()
+        }
+    }
+    
+    private func updateUI()
+    {
+        // reset any existing tweet information
+        tweetTextLabel?.attributedText = nil
+        tweetScreenNameLabel?.text = nil
+        tweetProfileImageView?.image = nil
+        tweetCreatedLabel?.text = nil
+        
+        // load new information from our tweet (if any)
+        if let tweet = self.tweet
+        {
+            tweetTextLabel?.text = tweet.text
+            if tweetTextLabel?.text != nil  {
+                for _ in tweet.media {
+                    tweetTextLabel.text! += " 📷"
+                }
+            }
+            
+            tweetScreenNameLabel?.text = "\(tweet.user)" // tweet.user.description
+            
+            if let profileImageURL = tweet.user.profileImageURL {
+                if let imageData = try? Data(contentsOf: profileImageURL) { // blocks main thread!
+                    tweetProfileImageView?.image = UIImage(data: imageData)
+                }
+            }
+            
+            let formatter = DateFormatter()
+            if  Date().timeIntervalSince(tweet.created) > 24*60*60 {
+                formatter.dateStyle = DateFormatter.Style.short
+            } else {
+                formatter.timeStyle = DateFormatter.Style.short
+            }
+            tweetCreatedLabel?.text = formatter.string(from: tweet.created)
+        }
+        
+    }
+}
